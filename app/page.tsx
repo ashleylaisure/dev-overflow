9f156b30d1bb7930b77d0ae91bb4f1acbd371bcb
Ashley Laisure <ashley.laisure@gmail.com>
2025-07-23 15:35:24 -0400

    Assets & Metadata setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Welcome to Next.js!</h1>
+      <h1 className="text-3xl font-space-grotesk">Welcome to Next.js(space)!</h1>
+      <h1 className="text-3xl font-inter">Welcome to Next.js (inter)!</h1>
+      <h1 className="text-3xl">Welcome to Next.js (inter)!</h1>
     </div>
   );
 };
