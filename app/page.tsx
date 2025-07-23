7a7e788a45d4d224ebe65ef0f313b5c1d06a8b8b
Ashley Laisure <ashley.laisure@gmail.com>
2025-07-23 14:18:57 -0400

    Implement ESLint & Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-
-
 const Home = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold">Welcome to Next.js!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
